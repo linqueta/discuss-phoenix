4256db9c0b05db49540f45674fdc1fdfda4bf49f
linqueta <lincolnrodrs@gmail.com>
2019-08-19 20:24:52 -0300

    add index screen and create route

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,6 +3,12 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+
+    render conn, "index.html", topics: topics
+  end
+
   def new(conn, params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
@@ -10,5 +16,14 @@ defmodule Discuss.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} -> render conn, "new.html", changeset: changeset
+    end
   end
 end

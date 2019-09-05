7f1d53bf21766a98567fdf5aec2f63d42f69886d
linqueta <lincolnrodrs@gmail.com>
2019-09-05 07:44:55 -0300

    insert comment through websocket

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,17 +1,29 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
-  alias Discuss.Topic
+  alias Discuss.{Topic, Comment}
 
   def join("comments:" <> topic_id, _params, socket) do
     topic_id = String.to_integer(topic_id)
     topic = Repo.get(Topic, topic_id)
 
-    {:ok, %{}, socket}
+    {:ok, %{}, assign(socket, :topic, topic)}
   end
 
-  def handle_in(name, message, socket) do
-    IO.inspect(name)
-    {:reply, :ok, socket}
+  def handle_in(name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+
+    changeset = topic
+      |> build_assoc(:comments)
+      |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, comment} -> reply_handled(:ok, socket)
+      {:error, _} -> reply_handled({:error, %{errors: changeset}}, socket)
+    end
+  end
+
+  defp reply_handled(content, socket) do
+    {:reply, content, socket }
   end
 end
